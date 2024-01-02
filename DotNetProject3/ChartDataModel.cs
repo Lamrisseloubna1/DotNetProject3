2ed41ea700657c325ed921370be316de5f52cf3c
Lamrisseloubna1 <lamrisse.loubna245@gmail.com>
2024-01-02 02:16:14 +0100

    Ajou de ChartDataModel

---
+++ DotNetProject3/ChartDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetProject3
+{
+    public class ChartDataModel
+    {
+        public string Nom_filiere { get; set; }
+        public int NombreEtudiants { get; set; }
+    }
+
+}
